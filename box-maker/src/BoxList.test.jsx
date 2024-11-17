dfc7ba16be4ab67680257c5deab1e792efa0614e
Moshe Swed <mswed@beapot.com>
2024-11-17 12:39:42 -0500

    passed all tests

--- box-maker/src/BoxList.test.jsx
+++ box-maker/src/BoxList.test.jsx
@@ -1,7 +1,27 @@
 import { it, expect } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import BoxList from './BoxList';
 
+const createBox = (boxes) => {
+  const { queryByText, getByLabelText } = boxes;
+  const width = getByLabelText('Width');
+  const height = getByLabelText('Height');
+  const color = getByLabelText('Color');
+  const addBtn = queryByText('Create Box');
+
+  // No boxes should be found on the page
+  expect(queryByText('X')).not.toBeInTheDocument();
+
+  // Change the form values
+  fireEvent.change(width, { target: { value: '100' } });
+  fireEvent.change(height, { target: { value: '100' } });
+  fireEvent.change(color, { target: { value: 'pink' } });
+
+  // Click the button
+  fireEvent.click(addBtn);
+
+  return [width, height, color];
+};
 it('Renders without crashing', async () => {
   render(<BoxList />);
 });
@@ -9,4 +29,40 @@ it('Renders without crashing', async () => {
 it('Matches snapshot', async () => {
   const { asFragment } = render(<BoxList />);
   expect(asFragment()).toMatchSnapshot();
+});
+
+it('Addes a box', async () => {
+  const boxes = render(<BoxList />);
+
+  // No boxes should be found on the page
+  expect(boxes.queryByText('X')).not.toBeInTheDocument();
+
+  const [width, height, color] = createBox(boxes);
+
+  // Check that the box is there
+  const removeBtn = boxes.queryByText('X');
+  expect(removeBtn).toBeInTheDocument();
+  const box = removeBtn.previousSibling;
+  expect(box).toHaveStyle(`
+    width: 100px;
+    height: 100px;
+    backgroundColor: 'pink';
+    display: inline-block;
+    margin: 5px;`);
+
+  // Check that inputs are clear
+  expect(width.value).toBe('');
+  expect(height.value).toBe('');
+  expect(color.value).toBe('');
+});
+
+it('Removes a box', async () => {
+  const boxes = render(<BoxList />);
+  expect(boxes.queryByText('X')).not.toBeInTheDocument();
+  createBox(boxes);
+
+  const removeBtn = boxes.queryByText('X');
+  expect(removeBtn).toBeInTheDocument();
+  fireEvent.click(removeBtn);
+  expect(boxes.queryByText('X')).not.toBeInTheDocument();
 });
